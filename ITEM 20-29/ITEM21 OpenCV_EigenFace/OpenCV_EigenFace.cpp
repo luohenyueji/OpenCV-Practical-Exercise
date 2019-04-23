ab7760782ac4a6e8734c2d6a0d4a0d6dcf66baa3
luohenyueji <wangyi0734@hotmail.com>
2019-04-23 20:06:04 +0800

    add OpenCV_EigenFace

---
+++ ITEM 20-29/ITEM21 OpenCV_EigenFace/OpenCV_EigenFace.cpp
@@ -0,0 +1,223 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <opencv2/core/core.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include "dirent.h"
+#include <stdlib.h>
+#include <time.h>
+
+using namespace cv;
+using namespace std;
+
+#define MAX_SLIDER_VALUE 255
+//主成分个数
+#define NUM_EIGEN_FACES 10
+
+// Weights for the different eigenvectors
+int sliderValues[NUM_EIGEN_FACES];
+
+// Matrices for average (mean) and eigenvectors
+Mat averageFace;
+vector<Mat> eigenFaces;
+
+// Read jpg files from the directory
+void readImages(string dirName, vector<Mat> &images)
+{
+	cout << "Reading images from " << dirName;
+
+	// Add slash to directory name if missing
+	if (!dirName.empty() && dirName.back() != '/')
+	{
+		dirName += '/';
+	}
+
+	DIR *dir;
+	struct dirent *ent;
+	int count = 0;
+
+	//image extensions 图像后缀
+	string imgExt = "jpg";
+	vector<string> files;
+
+	if ((dir = opendir(dirName.c_str())) != NULL)
+	{
+		/* print all the files and directories within directory */
+		while ((ent = readdir(dir)) != NULL)
+		{
+			if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+			{
+				continue;
+			}
+			string fname = ent->d_name;
+
+			if (fname.find(imgExt, (fname.length() - imgExt.length())) != std::string::npos)
+			{
+				string path = dirName + fname;
+				Mat img = imread(path);
+				if (!img.data)
+				{
+					cout << "image " << path << " not read properly" << endl;
+				}
+				else
+				{
+					// Convert images to floating point type 保存图像
+					img.convertTo(img, CV_32FC3, 1 / 255.0);
+					images.push_back(img);
+
+					// A vertically flipped image is also a valid face image.
+					// So lets use them as well. 翻转图像
+					Mat imgFlip;
+					flip(img, imgFlip, 1);
+					images.push_back(imgFlip);
+				}
+			}
+		}
+		closedir(dir);
+	}
+
+	// Exit program if no images are found
+	if (images.empty())
+	{
+		exit(EXIT_FAILURE);
+	}
+	cout << "... " << images.size() / 2 << " files read" << endl;
+}
+
+// Create data matrix from a vector of images 创建图像矩阵
+static  Mat createDataMatrix(const vector<Mat> &images)
+{
+	cout << "Creating data matrix from images ...";
+
+	// Allocate space for all images in one data matrix.
+	// The size of the data matrix is
+	//
+	// ( w  * h  * 3, numImages )
+	//
+	// where,
+	//
+	// w = width of an image in the dataset.
+	// h = height of an image in the dataset.
+	// 3 is for the 3 color channels.
+
+	Mat data(static_cast<int>(images.size()), images[0].rows * images[0].cols * 3, CV_32F);
+
+	// Turn an image into one row vector in the data matrix
+	for (unsigned int i = 0; i < images.size(); i++)
+	{
+		// Extract image as one long vector of size w x h x 3 重新设置通道行数大小
+		//reshape函数第一个参数通道数，第二个参数行数，和python中reshape函数不一样。
+		Mat image = images[i].reshape(1, 1);
+
+		// Copy the long vector into one row of the destm
+		image.copyTo(data.row(i));
+	}
+
+	cout << " DONE" << endl;
+	return data;
+}
+
+// Calculate final image by adding weighted
+// EigenFaces to the average face.
+void createNewFace(int, void *)
+{
+	// Start with the mean image
+	Mat output = averageFace.clone();
+
+	// Add the eigen faces with the weights
+	for (int i = 0; i < NUM_EIGEN_FACES; i++)
+	{
+		// OpenCV does not allow slider values to be negative.
+		// So we use weight = sliderValue - MAX_SLIDER_VALUE / 2
+		double weight = sliderValues[i] - MAX_SLIDER_VALUE / 2;
+		//获得输出图像
+		output = output + eigenFaces[i] * weight;
+	}
+
+	resize(output, output, Size(), 2, 2);
+
+	imshow("Result", output);
+}
+
+// Reset slider values
+void resetSliderValues(int event, int x, int y, int flags, void* userdata)
+{
+	if (event == EVENT_LBUTTONDOWN)
+	{
+		for (int i = 0; i < NUM_EIGEN_FACES; i++)
+		{
+			sliderValues[i] = 128;
+			setTrackbarPos("Weight" + to_string(i), "Trackbars", MAX_SLIDER_VALUE / 2);
+		}
+
+		createNewFace(0, 0);
+	}
+}
+
+int main()
+{
+	// Directory containing images 用于获取平均图像目录
+	string dirName = "image/";
+
+	// Read images in the directory 从目录中读取图像
+	vector<Mat> images;
+	readImages(dirName, images);
+
+	// Size of images. All images should be the same size. 图像尺寸
+	Size sz = images[0].size();
+
+	// Create data matrix for PCA. 为PCA创建数据矩阵
+	Mat data = createDataMatrix(images);
+
+	// Calculate PCA of the data matrix 计算PCA
+	cout << "Calculating PCA ...";
+	//提取十个主成分
+	PCA pca(data, Mat(), PCA::DATA_AS_ROW, NUM_EIGEN_FACES);
+	cout << " DONE" << endl;
+
+	// Extract mean vector and reshape it to obtain average face 获得均值图
+	//reshape函数第一个参数通道数，第二个参数行数，和python中reshape函数不一样。
+	averageFace = pca.mean.reshape(3, sz.height);
+
+	// Find eigen vectors. 寻找eign向量
+	Mat eigenVectors = pca.eigenvectors;
+
+	// Reshape Eigenvectors to obtain EigenFaces 获得Eign图
+	for (int i = 0; i < NUM_EIGEN_FACES; i++)
+	{
+		Mat eigenFace = eigenVectors.row(i).reshape(3, sz.height);
+		eigenFaces.push_back(eigenFace);
+	}
+
+	// Show mean face image at 2x the original size
+	Mat output;
+	//图像长宽都变成原来的两倍
+	resize(averageFace, output, Size(), 2, 2);
+
+	namedWindow("Result", CV_WINDOW_AUTOSIZE);
+	imshow("Result", output);
+
+	// Create trackbars
+	namedWindow("Trackbars", CV_WINDOW_AUTOSIZE);
+	for (int i = 0; i < NUM_EIGEN_FACES; i++)
+	{
+		//滑动窗格
+		sliderValues[i] = MAX_SLIDER_VALUE / 2;
+		createTrackbar("Weight" + to_string(i), "Trackbars", &sliderValues[i], MAX_SLIDER_VALUE, createNewFace);
+	}
+
+	// You can reset the sliders by clicking on the mean image.
+	setMouseCallback("Result", resetSliderValues);
+
+	cout << "Usage:" << endl
+		<< "\tChange the weights using the sliders" << endl
+		<< "\tClick on the result window to reset sliders" << endl
+		<< "\tHit ESC to terminate program." << endl;
+
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
--- README.md
+++ README.md
@@ -4,14 +4,14 @@ My CSDN blog: https://blog.csdn.net/LuohenYJ
 Environment: OpenCV(>3.43), C++11, Python3
 
 ---------------------------
-|Items|项目|Model|
+|Items|项目|Resources|
 |------|------|:------:|
 |[1age_gender](https://www.learnopencv.com/age-gender-classification-using-opencv-deep-learning-c-python/)|[1基于深度学习识别人脸性别和年龄](https://blog.csdn.net/LuohenYJ/article/details/88134634)|[Model](https://github.com/luohenyueji/OpenCV-Model-ZOO/tree/master/OpenCV-Practical-Exercise-Model)|
 |[2OpenCV_dlib_face_detection](https://www.learnopencv.com/face-detection-opencv-dlib-and-deep-learning-c-python/)|[2人脸识别算法对比](https://blog.csdn.net/LuohenYJ/article/details/88183455)|[Model](https://github.com/luohenyueji/OpenCV-Model-ZOO/tree/master/OpenCV-Practical-Exercise-Model)|
-|[3invisibility_cloak](https://www.learnopencv.com/invisibility-cloak-using-color-detection-and-segmentation-with-opencv/)|[3透明斗篷](https://blog.csdn.net/LuohenYJ/article/details/88218789)|-|
+|[3invisibility_cloak](https://www.learnopencv.com/invisibility-cloak-using-color-detection-and-segmentation-with-opencv/)|[3透明斗篷](https://blog.csdn.net/LuohenYJ/article/details/88218789)|[Video](https://github.com/luohenyueji/OpenCV-ML-Model/tree/master/OpenCV-Practical-Exercise-Model/videos)|
 |[4color spaces in OpenCV](https://www.learnopencv.com/color-spaces-in-opencv-cpp-python/)|[4OpenCV中的颜色空间](https://blog.csdn.net/LuohenYJ/article/details/88237008)|-|
 |[5text_detection](https://www.learnopencv.com/deep-learning-based-text-detection-using-opencv-c-python/)|[5基于深度学习的文本检测](https://blog.csdn.net/LuohenYJ/article/details/88248984)|[Model](https://github.com/luohenyueji/OpenCV-Model-ZOO/tree/master/OpenCV-Practical-Exercise-Model)|
-|[6video_stabilization](https://www.learnopencv.com/video-stabilization-using-point-feature-matching-in-opencv/)|[6基于特征点匹配的视频稳像](https://blog.csdn.net/LuohenYJ/article/details/88355444)|-|
+|[6video_stabilization](https://www.learnopencv.com/video-stabilization-using-point-feature-matching-in-opencv/)|[6基于特征点匹配的视频稳像](https://blog.csdn.net/LuohenYJ/article/details/88355444)|[Video](https://github.com/luohenyueji/OpenCV-ML-Model/tree/master/OpenCV-Practical-Exercise-Model/videos)|
 |[7yolov3_detect](https://www.learnopencv.com/deep-learning-based-object-detection-using-yolov3-with-opencv-python-c/)|[7使用YOLOv3和OpenCV进行基于深度学习的目标检测](https://blog.csdn.net/LuohenYJ/article/details/88537253)|[Model](https://pjreddie.com/media/files/yolov3.weights)|
 |[8training_YOLOv3](https://www.learnopencv.com/training-yolov3-deep-learning-based-custom-object-detector/)|[8深度学习目标检测网络YOLOv3的训练](https://blog.csdn.net/LuohenYJ/article/details/88581335)|-|
 |[9centroid_blob](https://www.learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/)|[9使用OpenCV寻找平面图形的质心](https://blog.csdn.net/LuohenYJ/article/details/88599334)|-|
@@ -21,8 +21,9 @@ Environment: OpenCV(>3.43), C++11, Python3
 |[13Mask R-CNN in OpenCV](https://www.learnopencv.com/deep-learning-based-object-detection-and-instance-segmentation-using-mask-r-cnn-in-opencv-python-c/)|[13OpenCV中使用Mask R-CNN进行对象检测和实例分割](https://blog.csdn.net/LuohenYJ/article/details/88978485)|[Model](http://download.tensorflow.org/models/object_detection/mask_rcnn_inception_v2_coco_2018_01_28.tar.gz)|
 |[14OpenCV_SingleTracker](https://www.learnopencv.com/object-tracking-using-opencv-cpp-python/)|[14使用OpenCV实现单目标跟踪](https://blog.csdn.net/LuohenYJ/article/details/89029816)|-|
 |[15GOTURN_SingleTracker](https://www.learnopencv.com/goturn-deep-learning-based-object-tracking/)|[15基于深度学习的目标跟踪算法GOTURN](https://blog.csdn.net/LuohenYJ/article/details/89083351)|[Model](https://github.com/spmallick/goturn-files)|
-|[16OpenCV_MultiTracker](https://www.learnopencv.com/multitracker-multiple-object-tracking-using-opencv-c-python/)|[16使用OpenCV实现多目标跟踪](https://blog.csdn.net/LuohenYJ/article/details/89105370)|-|
+|[16OpenCV_MultiTracker](https://www.learnopencv.com/multitracker-multiple-object-tracking-using-opencv-c-python/)|[16使用OpenCV实现多目标跟踪](https://blog.csdn.net/LuohenYJ/article/details/89105370)|[Video](https://github.com/luohenyueji/OpenCV-ML-Model/tree/master/OpenCV-Practical-Exercise-Model/videos)|
 |[17OpenCV_Colorization](https://www.learnopencv.com/convolutional-neural-network-based-image-colorization-using-opencv/)|[17基于卷积神经网络的OpenCV图像着色](https://blog.csdn.net/LuohenYJ/article/details/89186195)|[Model](https://people.eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/)|
 |[18OpenCV_Homography](https://www.learnopencv.com/homography-examples-using-opencv-python-c/)|[18Opencv中的单应性矩阵Homography](https://blog.csdn.net/LuohenYJ/article/details/89334249)|-|
 |[19OpenCV_Align](https://www.learnopencv.com/image-alignment-feature-based-using-opencv-c-python/)|[19使用OpenCV实现基于特征的图像对齐](https://blog.csdn.net/LuohenYJ/article/details/89351966)|-|
 |[20OpenCV_ECCAlignment](https://www.learnopencv.com/image-alignment-ecc-in-opencv-c-python/)|[20使用OpenCV实现基于增强相关系数最大化的图像对齐](https://blog.csdn.net/LuohenYJ/article/details/89455660)|-|
+|[21OpenCV_EigenFace](https://www.learnopencv.com/eigenface-using-opencv-c-python/)|[21使用OpenCV的Eigenface](https://blog.csdn.net/LuohenYJ/article/details/89479531)|[Image](https://github.com/luohenyueji/OpenCV-ML-Model/tree/master/OpenCV-Practical-Exercise-Model/images)|
